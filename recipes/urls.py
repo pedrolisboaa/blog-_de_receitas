fa0c9e3879c7742e36c410899e5dff5bf591f5e3
pedrolisboaa <phlisboa2000@gmail.com>
2023-04-06 14:39:18 -0300

    Atualizando

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import index
+from .views import index, recipes
 
 urlpatterns = [
     path('', index, name='index'),
+    path('recipes/<int:id>/', recipes, name='recipes'),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
+from utils.factory import make_recipe
 
 
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html',)
 
+
+    return render(request, 'index.html', context={'recipes': [make_recipe() for _ in range(10)]} )
+
+
+def recipes(request, id):
+    return render(request, 'recipes.html')
