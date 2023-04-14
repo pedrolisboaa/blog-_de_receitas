a0114b5917370ace650ea00c6371e1411435c44f
pedrolisboaa <phlisboa2000@gmail.com>
2023-04-14 16:44:19 -0300

    subindo banco

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category, Recipe
 
 # Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'category', 'created_at')
+
+
--- recipes/models.py
+++ recipes/models.py
@@ -5,6 +5,8 @@ from django.contrib.auth.models import User
 
 class Category(models.Model):
     title = models.CharField(max_length=65)
+    def __str__(self):
+        return self.title
 
 
 
@@ -23,6 +25,7 @@ class Recipe(models.Model):
     is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='recipes/cover/%Y/%m/%d')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
-    author = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
-
+    def __str__(self):
+        return self.title
