2e8d5710e7947315739d6387c325e42b0f1ea504
pedrolisboaa <phlisboa2000@gmail.com>
2023-04-27 16:33:11 -0300

    finalizando teste de url

--- recipes/tests.py
+++ recipes/tests.py
@@ -12,3 +12,7 @@ class RecipeURLsTest(TestCase):
     def test_recipe_category_url_is_correct(self):
         url = reverse('recipes:category', kwargs={"category_id": 1})
         self.assertEqual(url, '/recipes/category/1/')
+
+    def test_recipe_cdetail_url_is_correct(self):
+        url = reverse('recipes:recipe', kwargs={"id": 1})
+        self.assertEqual(url, '/recipes/1/')
