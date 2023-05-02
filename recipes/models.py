8f7eb89f305764484a2ff008997d29d84c2d5625
pedrolisboaa <phlisboa2000@gmail.com>
2023-05-02 17:34:06 -0300

    subindo

--- recipes/models.py
+++ recipes/models.py
@@ -3,17 +3,18 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+
 class Category(models.Model):
     title = models.CharField(max_length=65)
+
     def __str__(self):
         return self.title
-
 
 
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
     description = models.CharField(max_length=165)
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True)
     preparation_time = models.IntegerField()
     preparation_time_unit = models.CharField(max_length=65)
     servings = models.IntegerField()
@@ -23,8 +24,8 @@ class Recipe(models.Model):
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
     is_published = models.BooleanField(default=False)
-    cover = models.ImageField(upload_to='recipes/cover/%Y/%m/%d', blank=True, default='')
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, default=None)
+    cover = models.ImageField(upload_to='recipes/cover/%Y/%m/%d', blank=True, default='')  # noqa: E501
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, default=None)  # noqa: E501
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
