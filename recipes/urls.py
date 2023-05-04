853499ee2b63e43a746a214e161dfbaf2dbb7e99
pedrolisboaa <phlisboa2000@gmail.com>
2023-05-04 17:29:33 -0300

    subindo

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import index, recipes, category
+from .views import index, recipes, category, search
 
 
 app_name = 'recipes'
 
 urlpatterns = [
     path('', index, name='home'),
+    path('recipes/search/', search, name='search'),
     path('recipes/<int:id>/', recipes, name='recipe'),
     path('recipes/category/<int:category_id>/', category, name='category'),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404
-from utils.factory import make_recipe
 from .models import Recipe
-
 
 
 # Create your views here.
@@ -11,13 +9,11 @@ def index(request):
         is_published=True,
     ).order_by('-id')
 
-    return render(request, 'index.html', context={'recipes': recipes} )
+    return render(request, 'index.html', context={'recipes': recipes})
 
 
 def category(request, category_id):
-    
-    recipe = get_list_or_404(Recipe,category__id=category_id, is_published=True, )
-    
+    recipe = get_list_or_404(Recipe, category__id=category_id, is_published=True)  # noqa: E501
     return render(request, 'category.html', context={
         'recipes': recipe,
         'title': f'CATEGORY | {recipe[0].category.title}'
@@ -28,7 +24,12 @@ def recipes(request, id):
     recipe = get_object_or_404(Recipe, pk=id, is_published=True)
 
     return render(request, 'recipes.html', context={
-        'recipe':recipe,
-        'is_detail_page':True,
+        'recipe': recipe,
+        'is_detail_page': True,
         })
-    
+
+
+def search(request):
+    search_term = request.GET.get('q')
+
+    return render(request, 'searchpage.html')
